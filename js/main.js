3c432c82198db79dec397694f849b956ea929f72
developernodir11 <usmonovnodir760@gmail.com>
2025-05-07 09:04:14 +0500

    oooooooo

--- js/main.js
+++ js/main.js
@@ -1,25 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const toggleBtn = document.getElementById('toggleBtn');
-  const navList = document.querySelector('.nav-list');
-  
-  toggleBtn.addEventListener('click', function() {
-    navList.classList.toggle('active');
-    const icon = this.querySelector('i');
-    if (navList.classList.contains('active')) {
-      icon.classList.remove('ri-menu-line');
-      icon.classList.add('ri-close-line');
-    } else {
-      icon.classList.remove('ri-close-line');
-      icon.classList.add('ri-menu-line');
-    }
-  });
-
-  document.addEventListener('click', function(e) {
-    if (!e.target.closest('header') && navList.classList.contains('active')) {
-      navList.classList.remove('active');
-      const icon = toggleBtn.querySelector('i');
-      icon.classList.remove('ri-close-line');
-      icon.classList.add('ri-menu-line');
-    }
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    document.getElementById("loader-wrapper").style.display = "none";
+    document.getElementById("main-content").style.display = "block";
+  }, 2000);
+});
+document.getElementById("scrollBtn").addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
   });
 });
+//   toggleBtn.addEventListener('click', function() {
+//     navList.classList.toggle('active');
+//     const icon = this.querySelector('i');
+//     if (navList.classList.contains('active')) {
+//       icon.classList.remove('ri-menu-line');
+//       icon.classList.add('ri-close-line');
+//     } else {
+//       icon.classList.remove('ri-close-line');
+//       icon.classList.add('ri-menu-line');
+//     }
+//   });
+
+//   document.addEventListener('click', function(e) {
+//     if (!e.target.closest('header') && navList.classList.contains('active')) {
+//       navList.classList.remove('active');
+//       const icon = toggleBtn.querySelector('i');
+//       icon.classList.remove('ri-close-line');
+//       icon.classList.add('ri-menu-line');
+//     }
+//   });
+// });
