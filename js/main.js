bd2e5ffbf8b236978b47647ef165a3302e2733e2
developernodir11 <usmonovnodir760@gmail.com>
2025-05-08 12:24:42 +0500

    yumpi

--- js/main.js
+++ js/main.js
@@ -1,33 +1,56 @@
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("load", function () {
+  const loaderWrapper = document.getElementById("loader-wrapper");
   setTimeout(() => {
-    document.getElementById("loader-wrapper").style.display = "none";
-    document.getElementById("main-content").style.display = "block";
-  }, 2000);
+    loaderWrapper.style.display = "none";
+  }, 2000); 
 });
-document.getElementById("scrollBtn").addEventListener("click", () => {
+document.getElementById('dark-mode-toggle').addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+});
+
+
+document.getElementById('hamburger').addEventListener('click', () => {
+  document.querySelector('nav ul').classList.toggle('active');
+});
+const backToTopBtn = document.getElementById("backToTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+});
+
+backToTopBtn.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
-    behavior: "smooth",
+    behavior: "smooth"
   });
 });
-//   toggleBtn.addEventListener('click', function() {
-//     navList.classList.toggle('active');
-//     const icon = this.querySelector('i');
-//     if (navList.classList.contains('active')) {
-//       icon.classList.remove('ri-menu-line');
-//       icon.classList.add('ri-close-line');
-//     } else {
-//       icon.classList.remove('ri-close-line');
-//       icon.classList.add('ri-menu-line');
-//     }
-//   });
+let modeBtn = document.querySelector(".site_moon");
 
-//   document.addEventListener('click', function(e) {
-//     if (!e.target.closest('header') && navList.classList.contains('active')) {
-//       navList.classList.remove('active');
-//       const icon = toggleBtn.querySelector('i');
-//       icon.classList.remove('ri-close-line');
-//       icon.classList.add('ri-menu-line');
-//     }
-//   });
-// });
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "./images/sun.svg";
+  } else {
+    this.firstElementChild.src = "./images/moon.svg";
+  }
+  document.body.classList.toggle("dark");
+});
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
